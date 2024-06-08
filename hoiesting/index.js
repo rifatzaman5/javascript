bfe3fa3fd18fe1aaac07d57db7f27589fc78dd00
Rifat Zaman <rzaman@tkbench.com>
2024-06-08 15:44:13 +0500

    javascript series

---
+++ hoiesting/index.js
@@ -0,0 +1,26 @@
+// add();
+// console.log(x);
+// console.log(add);
+// // A function that logs "hello world" to the console.
+// var add = () => {
+//   console.log("helo world");
+// }
+// add();
+// console.log(x);
+// console.log(add);
+// // A function that logs "hello world" to the console.
+// function add() {
+//   console.log("helo world");
+// }
+
+// var x = 10;
+// // A function that logs "hello world" to the console.
+// function add() {
+//   console.log("helo world");
+// }
+// add();
+// console.log(x);
+// console.log(add);
+
+
+
